2af668f6a73e3884fd3d4fd7a7c653c296f231d7
Fabian Riegsinger <fabian.riegsinger@web.de>
2024-10-07 07:50:21 +0200

    Somewhat working Dockerfile. Still problem with ui

--- start_install_elec_react_frontend.sh
+++ start_install_elec_react_frontend.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Check if Node.js is installed
+if command -v node >/dev/null 2>&1; then
+    echo "Node.js is installed"
+    # You can also print the Node.js version
+    node --version
+else
+    echo "Node.js is not installed"
+    exit 1
+fi
+
+# Check if npm is installed
+if command -v npm >/dev/null 2>&1; then
+    echo "npm is installed"
+    # You can also print the npm version
+    npm --version
+else
+    echo "npm is not installed"
+    exit 1
+fi
+
 # Check if the folder exists
 if [ -d "electron-react-frontend/node_modules" ]; then
     echo "Project dependencies seem to be installed.."
